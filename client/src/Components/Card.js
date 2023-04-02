4580f45f9e1017d785168414afc313a73b74c20f
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2023-04-02 18:29:23 +0530

    Home page design completed

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,14 +1,16 @@
 export default function Card(props) {
   return (
-    <div className="flex">
-      <div className="h-80 w-72 bg-gradient-to-r from-blue-100 to-indigo-200 flex flex-col justify-around items-center rounded-xl flex-wrap">
+    <div className="flex hover:scale-110 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)] rounded-xl">
+      <div className="h-80 w-72 bg-gradient-to-r from-slate-200 to-gray-300 flex flex-col justify-around items-center rounded-xl flex-wrap">
         <img
           src={require(`../Images/${props.logo}`)}
           alt="logo"
           className="h-48 w-48 object-contain"
         />
-        <div className="border-2 rounded-lg  h-10 w-36 cursor-pointer  text-center flex items-center justify-center">
-          <div className="text-slate-900 font-bold text-lg">{props.title}</div>
+        <div className="bg-gradient-to-r from-indigo-900 to-fuchsia-700 border-2 border-white rounded-lg  h-10 w-36 cursor-pointer  text-center flex items-center justify-center hover:opacity-60">
+          <div className="text-white font-semibold text-lg font-grotesque">
+            {props.title}
+          </div>
         </div>
       </div>
     </div>
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,7 +2,9 @@ import "../glow.css";
 export default function Navbar() {
   return (
     <div className="">
-      <div className="text-6xl text-white font-bold text-glow">InfoCrawler</div>
+      <div className="text-6xl text-white font-bold font-poppins text-glow">
+        InfoCrawler
+      </div>
     </div>
   );
 }
