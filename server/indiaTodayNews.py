58f828c60348c4c411bcfbf84dcc5b48ef870211
bajajdivya <bajajdivya02@gmail.com>
2023-04-07 11:39:35 +0530

    making the size of array same

--- server/indiaTodayNews.py
+++ server/indiaTodayNews.py
@@ -21,6 +21,9 @@ def newsindiatodayscrapper():
     for p in p_tags:
         p_text.append(p.text)
 
+    p_text = p_text[:11]
+    h2_text = h2_text[:11]
+
     data = {'Heading': h2_text, 'Paragraph': p_text}
     df = pd.DataFrame(data)
 
